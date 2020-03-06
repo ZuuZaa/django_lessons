3383184b7ade4b99dd9a592a077028a4c9ca502f
ZuuZaa <zumbaghirova@gmail.com>
2020-03-06 16:39:42 +0400

    commit

--- myproject/main/admin.py
+++ myproject/main/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Main
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Main)
+admin.site.register(RelatedPerson)
+admin.site.register(Note)
+admin.site.register(Customer)
 
--- myproject/main/models.py
+++ myproject/main/models.py
@@ -14,4 +14,69 @@ class Main(models.Model):
 # for admin panel
     def __str__(self):
         return self.name + " | " + str(self.pk)
+
+
+from django.db import models
+
+# Create your models here.
+
+class RelatedPerson(models.Model):
+
+    first_name = models.CharField( max_length=50 )
+    last_name = models.CharField( max_length=50 )
+    fathers_name = models.CharField( max_length=50 )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+class Note(models.Model):
+
+    name = models.CharField( max_length=50 )
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+
+    first_name = models.CharField( max_length=50 )
+    last_name = models.CharField( max_length=50 )
+    fathers_name = models.CharField( max_length=50 )
+
+    related_person = models.ForeignKey(RelatedPerson, on_delete=models.CASCADE, null=True, blank=True)
+
+    phone_number_1 = models.CharField( max_length=50 )
+    phone_number_2 = models.CharField( max_length=50, null=True, blank=True)
+    email = models.EmailField(max_length=150)
+
+
+    class Sources(models.TextChoices):
+        FaceBook = '1', "Facebook"
+        Instagram = '2', "Instagram"
+        Google = '3', "Google"
+        acquainted = '4', "Tanış məsləhəti"
+
+    source = models.CharField(
+        max_length=2,
+        choices=Sources.choices,
+        default=Sources.acquainted,
+    )
+
+    note = models.ForeignKey(Note, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Statuses(models.TextChoices):
+        next = '5', "Növbəti"
+        cancel = '6', "İmtina et"
+        delay = '7', "Təxirə sal"
+
+    status = models.CharField(
+        max_length=2,
+        choices=Statuses.choices,
+        default=Statuses.next,
+    )
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
     
